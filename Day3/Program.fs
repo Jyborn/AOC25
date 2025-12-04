c325d2da41bde74cbd3045bbdf1ca60263e31670
Jyborn <alexander.jyborn@accenture.com>
2025-12-04 18:56:12 +0100

    day3 wip

---
+++ Day3/Program.fs
@@ -0,0 +1,13 @@
+﻿open System.IO
+
+let part1 input = 
+    200
+
+
+[<EntryPoint>]
+let main argv =
+    let line = File.ReadAllText("test.txt").Trim()
+
+    let result1 = part1 line
+    printfn "Result part1: %d" result1
+    0
