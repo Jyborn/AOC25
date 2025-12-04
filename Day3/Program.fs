45c7832c9f8ed8d6b1591e3aba2df84c52ab7f39
Jyborn <alexander.jyborn@accenture.com>
2025-12-04 19:05:01 +0100

    day3 wip

--- Day3/Program.fs
+++ Day3/Program.fs
@@ -1,13 +1,26 @@
 ﻿open System.IO
 
-let part1 input = 
-    200
+let getLargestTwoDigitValue (line:string) =
+    99
+
+let part1 (lines: string[]) = 
+    let largestValues =
+        lines
+        |> Array.map getLargestTwoDigitValue
+        
+    printfn "Largest values: %A" largestValues
+    largestValues |> Array.sumBy int64 
+
+let part2 (input: string[]) = 2000
 
 
 [<EntryPoint>]
 let main argv =
-    let line = File.ReadAllText("test.txt").Trim()
+    let lines = File.ReadAllLines("test.txt")
 
-    let result1 = part1 line
+    let result1 = part1 lines
     printfn "Result part1: %d" result1
+
+    let result2 = part2 lines
+    printfn "Result part1: %d" result2
     0
